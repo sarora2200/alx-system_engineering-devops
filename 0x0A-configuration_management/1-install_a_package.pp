c26e3d171954d25e07a042874aa0ea5d6a9e23c7
sarora2200 <sarora2200@gmail.com>
2023-12-22 12:50:24 -0800

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,25 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Install flask and required packages
+
+# Install python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install an especific version of flask (2.1.0) 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
