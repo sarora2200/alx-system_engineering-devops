87d90711734ddab0555e76b9256992059353dddc
sarora2200 <sarora2200@gmail.com>
2023-12-22 08:17:04 -0800

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Install a specific version of Flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  command  => '/usr/bin/pip3', # Specify the path to pip3 executable
+  provider => 'pip3',
 }
 
