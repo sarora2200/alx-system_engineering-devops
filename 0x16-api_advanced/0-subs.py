290fc265abb2ed80c4c832b027025164d37cd8f7
sarora2200 <sarora2200@gmail.com>
2024-02-13 12:08:09 -0800

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,11 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
+    response.raise_for_status()
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    try:
+        results = response.json().get("data")
+        return results.get("subscribers")
+    except ValueError:
+        return 0
